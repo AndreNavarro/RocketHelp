e1eb1f131fbf7b74cee59e8232b4ea7dce72d606
André Navarro <andre.navarro@bistwobis.com.br>
2022-07-20 19:29:50 -0300

    :sparkles: Added Register and Details screen with Header component - Ignite Lab

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import { THEME } from './src/styles/theme';
 import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
 import { Home } from './src/screens/Home';
+import { Register } from './src/screens/Register';
 
 export default function App() {
     const [fontsLoad] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -17,8 +18,8 @@ export default function App() {
                 backgroundColor="transparent"
                 translucent
             />
-            {/* Alterado SignIn -> Home para fácil visualização durante desenvolvimento  */}
-            {fontsLoad ? <Home /> : <Loading />}
+            {/* Alterado SignIn -> Register para fácil visualização durante desenvolvimento  */}
+            {fontsLoad ? <Register /> : <Loading />}
         </NativeBaseProvider>
     );
 }
