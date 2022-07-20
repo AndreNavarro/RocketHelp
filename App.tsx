b4b77c4c13526ee353b295596a27e8c7d6943d1c
André Navarro <andre.navarro@bistwobis.com.br>
2022-07-20 11:45:28 -0300

    :sparkles: Added Home screen with Filter component - Ignite Lab

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { THEME } from './src/styles/theme';
 
 import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
     const [fontsLoad] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -16,7 +17,8 @@ export default function App() {
                 backgroundColor="transparent"
                 translucent
             />
-            {fontsLoad ? <SignIn /> : <Loading />}
+            {/* Alterado SignIn -> Home para fácil visualização durante desenvolvimento  */}
+            {fontsLoad ? <Home /> : <Loading />}
         </NativeBaseProvider>
     );
 }
